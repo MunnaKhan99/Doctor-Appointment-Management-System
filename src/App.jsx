af534960291060cbcfef4bdf6fa9d13636299c79
MunnaKhan99 <munnakhan01329@gmail.com>
2025-09-02 01:36:46 +0600

    Create Login component, add react-router-dom and add nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+
+import Login from "./components/Login/Login";
+import Register from "./components/Login/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/login" replace />,
+  },
+  {
+    path: "login",
+    element: <Login />,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
 
 const App = () => {
-  return (
-    <div>
-        <p>this is app</p>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
